5beda87c618d4870321979b0b9a25ecb073e62b8
MD Sharif <srka780@gmail.com>
2025-05-21 19:13:34 +0600

    use prismamodule to popertismodule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+// import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -10,7 +10,7 @@ export default tseslint.config(
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+  // eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
--- src/properties/properties.module.ts
+++ src/properties/properties.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PropertiesService } from './properties.service';
 import { PropertiesController } from './properties.controller';
-import { PrismaClientService } from 'src/prisma_client/prisma_client.service';
+// import { PrismaClientService } from 'src/prisma_client/prisma_client.service';
+import { PrismaClientModule } from 'src/prisma_client/prisma_client.module';
 
 @Module({
+  imports: [PrismaClientModule],
   controllers: [PropertiesController],
-  providers: [PropertiesService, PrismaClientService],
+  providers: [PropertiesService],
 })
 export class PropertiesModule {}
