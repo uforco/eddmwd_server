f69e6256a475c4310c5c8f82bdc5c66a47f3cc62
Shahin <shahin@Jayeds-Mac-mini.local>
2025-05-21 06:03:40 +0600

    fixed problem

--- src/properties/dto/create-property.dto.ts
+++ src/properties/dto/create-property.dto.ts
@@ -1,25 +1,16 @@
+export enum AvailableFor {
+  Rent = 'Rent',
+  Sell = 'Sell',
+}
+
 export class CreatePropertyDto {
-  rooms: number   
-  size: number 
-  bathrooms: number
-  beds: number
-  available_for: string
-  short_desc: string
-  desc: string
-  location_id: string
-  owner_id: number
-}
-
-class Location{
-    address: string
-    city: string
-    postal_code: string
-    state: string
-    country: string
-}
-
-class Owner{
-    email: string
-    name: string
-    phone: string
+  rooms: number;
+  size: number;
+  bathrooms: number;
+  beds: number;
+  available_for: AvailableFor;
+  short_desc: string;
+  desc: string;
+  location_id: string;
+  owner_id: number;
 }
