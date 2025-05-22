2a468c021a9dc6527c656dfa4c3992998150a548
uforco <srka780@gmail.com>
2025-05-23 01:23:58 +0600

    fix apis cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors()
 
   const config = new DocumentBuilder()
           .setTitle('Prime')
