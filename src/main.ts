4422a455b7e7c7113fabd94f6c996e6cdf1dcfd9
uforco <srka780@gmail.com>
2025-05-23 01:15:56 +0600

    fix apis

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
           .build()
           
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/', app, document)
+  SwaggerModule.setup('/apis', app, document)
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
