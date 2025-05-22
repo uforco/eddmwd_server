fb4403d3d54df3db717733bbb62c1ebce630837d
uforco <srka780@gmail.com>
2025-05-22 22:52:47 +0600

    sagger api

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,8 @@ async function bootstrap() {
           .build()
   
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document)
+  SwaggerModule.setup('/', app, document)
   
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
